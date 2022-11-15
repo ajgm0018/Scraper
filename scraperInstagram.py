05fba4ba9f27423be227121e0eeda00f7590c9b1
Alberto J. Gutiérrez <ajgm0018@red.ujaen.es>
2022-11-15 13:21:39 +0100

    Add files via upload

---
+++ scraperInstagram.py
@@ -0,0 +1,103 @@
+# -- Librerias -- #
+import os.path
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# -- Variables -- #
+n_messages = 6
+
+# -- Funciones -- #
+def conf_chrome():
+    # Configuracion de chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Eliminamos la interfaz
+    chrome_options.add_argument("--no-sandbox")
+    
+    # Añadir path de chromedirver a la configuracion
+    homedir = os.path.expanduser("~")
+    webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
+    
+    # Eleccion de chrome como buscador
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    
+    return browser
+
+def login():
+    if len(sys.argv) != 3:
+        print("Introduce <usuario> <contraseña> de Twitter")
+        exit()
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+    
+    # Login de instagram
+    print("Accediendo a Instagram")
+    browser.get("https://www.instagram.com/login")
+    time.sleep(4)
+    
+    # Pop-up
+    print("Cruzando pop-up - Inicio sesión")
+    select = browser.find_element(By.XPATH, '//*[@class="_a9-- _a9_0"]')
+    select.click()
+    
+    # Credenciales
+    print("Accediendo a usuario - Pendiente")
+    select = browser.find_element(By.NAME, 'username')
+    select.send_keys(username)
+    select = browser.find_element(By.NAME, 'password')
+    select.send_keys(password)
+    time.sleep(3)
+      
+    # Click en siguiente
+    select = browser.find_element(By.XPATH, '//*[@id="loginForm"]')
+    select.click()
+    time.sleep(3)
+    print("Accediendo a usuario - Éxito")
+    
+    print("Validando popup - Dentro de sesión")
+    select = browser.find_element(By.XPATH, '//*[@class="_ac8f"]');
+    select.click()
+    time.sleep(2)
+    
+    # Entrar en mensajes
+    select = browser.find_element(By.XPATH, '//a[@href="/direct/inbox/"]')
+    select.click()
+    time.sleep(4)
+     
+    print("Accediendo a mensajes - Éxito") 
+
+def get_messages(n_messages):
+    
+    all_messages = []
+    
+    messages = browser.find_elements(By.XPATH, '//*[@class="_abm4"]');
+    print("Se han encontrado:", len(messages), "conversaciones")
+    
+    for n_messages in range (len(messages) - 1):
+        print("Procesando conversacion:",n_messages)
+        messages[n_messages].click()
+        time.sleep(2)
+        
+        tips = browser.find_elements(By.XPATH, '//div[@role="button"]')
+        
+        for n in range(len(tips) - 1):
+            if(tips[n].text != ""):
+                all_messages.append(tips[n].text)
+    
+    print("Recopilación de mensajes - Éxito")
+    return all_messages
+
+# ---- Main ---- #
+if __name__ == "__main__":
+    # Guardo el buscador
+    browser = conf_chrome() 
+    # Almacenamos credenciales
+    login()
+    # Obtenemos los mensajes
+    all_messages = get_messages(n_messages)
---
+++ scraperTwitter.py
@@ -0,0 +1,108 @@
+# -- Librerias -- #
+import os.path
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# -- Variables -- #
+n_messages = 3
+
+# -- Funciones -- #
+def conf_chrome():
+    # Configuracion de chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Eliminamos la interfaz
+    chrome_options.add_argument("--no-sandbox")
+    
+    # Añadir path de chromedirver a la configuracion
+    homedir = os.path.expanduser("~")
+    webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
+    
+    # Eleccion de chrome como buscador
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    
+    return browser
+
+
+def login():
+    if len(sys.argv) != 3:
+        print("Introduce <usuario> <contraseña> de Twitter")
+        exit()
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+    
+    # Login de twitter
+    print("Accediendo a Twitter")
+    browser.get("https://www.twitter.com/login")
+    time.sleep(4)
+        
+    # Credenciales
+    print("Accediendo a usuario - Pendiente")
+    select = browser.find_element(By.NAME, 'text')
+    select.click()
+    select.send_keys(username)
+    time.sleep(2)
+      
+    # Click en siguiente
+    select = browser.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
+    select.click()
+    time.sleep(2)
+    print("Accediendo a usuario - Éxito")
+    
+    # Enviar contraseña
+    print("Accediendo a contraseña - Pendiente")
+    select = browser.find_element(By.NAME, 'password')
+    select.send_keys(password)
+    time.sleep(2)
+    
+    # Click en entrar
+    select = browser.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')
+    select.click()
+    time.sleep(4)
+    print("Accediendo a contraseña - Éxito")  
+
+
+def get_messages(n_messages):
+    print("Accediendo a mensajes - Pendiente")
+    # Accedemos a los mensajes directos
+    browser.get("https://www.twitter.com/messages")
+    time.sleep(2)
+    print("Accediendo a mensajes - Éxito")
+    
+    all_messages = []
+    
+    # Solo los últimos 20 mensajes por conversación
+    for n_message in range (n_messages):
+        print("Mensaje [",n_message,"] - Realizando")
+        # Entrar
+        messages = browser.find_elements(By.XPATH, '//div[@data-testid="conversation"]')
+        messages[n_message].click()
+        time.sleep(2)
+        
+        # Mensajes
+        tips = browser.find_elements(By.XPATH, '//div[@role="presentation"]')
+        for n in range (len(tips)):
+            if tips[n].text != "":
+                all_messages.append(tips[n].text)
+        
+        # Salir
+        select = browser.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div/div/div/div[1]/div/div')
+        select.click()
+        time.sleep(2)
+        
+    return all_messages
+
+# ---- Main ---- #
+if __name__ == "__main__":
+    # Guardo el buscador
+    browser = conf_chrome() 
+    # Almacenamos credenciales
+    login()
+    # Obtenemos los mensajes
+    all_messages = get_messages(n_messages)
+    
+    print(f"{all_messages}")
