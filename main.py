c032a891445783cb1d0866b4bc5e9efa97764f2a
Alberto J. Gutiérrez <ajgm0018@red.ujaen.es>
2022-12-13 13:48:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ import pandas as pd
 import requests
 import json
 import random
+import configparser
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 
 # -- Variables -- #
 """La variable superusuario debe ser previamente configurada
 en la base de datos
 """
-superusario = "a@a.com"
-password = "alberto10"
-authbearer = "Bearer "
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+superusario = config['superuser']['superusario']
+password = config['superuser']['password']
+authbearer = config['superuser']['authbearer']
 
 # -- Funciones -- #
 
@@ -21,10 +28,10 @@ todas las redes sociales registradas y devolverlas
 """
 def almacenarRedesSociales():
     print("Almacenar redes sociales -- Comenzando...")
-    
+
     # Creando login form
     userForm = {'username':superusario, 'password':password}
-    
+
     # Obtener token
     session = requests.Session()
     token = session.post('http://localhost:5500/login', data=userForm)
@@ -33,15 +40,28 @@ def almacenarRedesSociales():
     tokenBearer = json.loads(jsonContent)
     tokenBearer = tokenBearer['access_token']
     tokenBearer = authbearer + tokenBearer
-    
+
     # Metodo get
     r = requests.get('http://localhost:5500/all-social-networks', headers=({'Authorization': tokenBearer}))
     print("Obtener redes sociales: ", r.status_code, r.reason)
     redesSociales = r.content
     redesSociales = json.loads(redesSociales)
-     
+
     print("Almacenar redes sociales -- Finalizado")
     return redesSociales
+
+"""Le pasas el archivo con la clave privada y el texto en bytes de la
+contraseña a desencriptar. Devuelve la clave en string
+"""
+def decrypt(filename, passcrypt):
+
+    with open(filename, "rb") as file:
+        private_key = RSA.importKey(file.read(), '')
+
+    rsa_cipher = PKCS1_OAEP.new(private_key)
+    decrypted_text = rsa_cipher.decrypt(passcrypt)
+
+    return decrypted_text.decode("utf-8")
 
 """Lanza un scraper, dependiendo de si la cuenta es de twitter o de
 instagram, para cada red social. Cada scraper almacenará el resultado
@@ -54,6 +74,10 @@ def scraper(redesSociales):
     for n in range(len(redesSociales)):
         id = (redesSociales[n]['id'])
         passw = (redesSociales[n]['encrypted_password'])
+        
+        # -- Desencriptar las contraseñas -- #
+        passw = decrypt('id_rsa', passw)
+        
         email = (redesSociales[n]['email'])
         name = (redesSociales[n]['name'])
         
@@ -66,9 +90,9 @@ def scraper(redesSociales):
             # if(name == 'instagram'): os.system('python3 scraperInstagram.py email passw id')
             # LOG acierto
         except:
-            print("No se ha completado la red social numero: ", n)
             # LOG fallo
- 
+            print("No se ha completado la red social numero: ", n)
+
 """Elimina todas las conversaciones privadas almacenados de la carpeta
 ./csv
 """           
@@ -77,12 +101,11 @@ def eliminar_csv():
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))  
 
-
 def log(tipo, id, finalizado):
     f = open('login.txt', 'w')
     f.write('Redsocial', tipo, ', id', id, 'a finalizado con:', finalizado)
     f.close
-            
+
 def prueba_añadir_score():
     
         # Creando login form
@@ -98,11 +121,10 @@ def prueba_añadir_score():
         
         # Prueba añadir scores    
         scores1 = {"score_1": round(random.uniform(0,1), 2),
-                   "score_2": round(random.uniform(0,1), 2),
-                   "score_3": round(random.uniform(0,1), 2),
-                   "score_4": round(random.uniform(0,1), 2)}
+                "score_2": round(random.uniform(0,1), 2),
+                "score_3": round(random.uniform(0,1), 2),
+                "score_4": round(random.uniform(0,1), 2)}
         session.post('http://localhost:5500/upload-scores/5', data=json.dumps(scores1), headers=({'Authorization': tokenBearer}))
-            
 
 # ---- Main ---- #
 if __name__ == "__main__":
@@ -112,6 +134,7 @@ if __name__ == "__main__":
     #Enviar a modelo
     #eliminar_csv()
     
-    
     #os.system('python3 scraperTwitter.py AlbertoJoseGuti Albertobaza10 1')
-    os.system('python3 scraperInstagram.py albertoguti1995 alberto10 2')
+    #os.system('python3 scraperInstagram.py albertoguti1995 alberto10 2')
+    
+    print("Buenos dias")
--- scraperTwitter.py
+++ scraperTwitter.py
@@ -165,4 +165,3 @@ if __name__ == "__main__":
         else:
             print("¿ERROR?!")
     
-   
